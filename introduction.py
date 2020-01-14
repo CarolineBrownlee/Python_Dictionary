d0d6bbfe4b5e022870296a5e6608cf794e58a47c
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-14 13:39:41 -0600

    finished intro and ready to start exercises

---
+++ introduction.py
@@ -0,0 +1,27 @@
+# Dictionaries are the new Object from JavaScript.
+
+# They are key/value pairs just like objects, but creating and accessing the key/value pairs can only be done with square-bracket notation.
+
+# You can create dictionaries in two ways:
+
+# 1. Create an empty dictionary
+# animal = dict()
+
+# And then add k/v pairs:
+# animal["name"] = "Kevin"
+# animal["breed"] = "Bulldog"
+# animal["age"] = 5
+# print(animal)
+
+# 2. Create the dictionary with k/v pairs and assign to variable
+# animal = {
+#     "name": "Kevin",
+#     "breed": "Bulldog",
+#     "age": 5
+# }
+# print(animal)
+
+# Dictionaries are iterable.
+
+# for (key, value) in animal.items():
+#     print(f"{key}: {value}")
